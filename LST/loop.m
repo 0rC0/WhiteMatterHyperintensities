b9170ae129ca4a5b76e836ee75c771cbb388801a
0rC0 <orco@paranoici.org>
2021-03-30 12:34:42 +0200

    directory_structure

--- LST/loop.m
+++
@@ -1,8 +0,0 @@
-% run LST_lpa with all FLAIRs
-% requires glob.m
-flairs = glob('/media/drive_s/AG/AG-Floeel-Imaging/02-User/AndreaDellOrco/WhiteMatterLesions/SMARTAGE_BIDS_test/flaircoreg/sub-SMART*/anat/sub-SMART*_FLAIR_roWarped.nii.gz')
-
-for i=1:length(flairs)
-disp(flairs{i})
-ps_LST_lpa(string(flairs{i}),'',1)
-end
